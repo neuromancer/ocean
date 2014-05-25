ca60104da14b8990a9a5f2d6f58871f8182aa937
Vagrant User <vagrant@vagrant.vm>
2014-05-25 08:33:29 -0700

    Fixed several bugs and add min size of event list

--- src/Printer.py
+++ src/Printer.py
@@ -17,28 +17,28 @@ class Printer:
 
   def preprocess(self, event):
 
-    r = set()
+    r = list()
     if isinstance(event, Call):
       #print event.name, event.param_types, event.ret, event.retaddr, event.retvalue
       (name, args) = event.GetTypedName()
 
       #r.add((name+":ret_addr",str(args[0])))
-      r.add((name+":ret_val",str(args[1])))
+      r.append((name+":ret_val",str(args[1])))
 
       for (index, arg) in enumerate(args[2:]):
-        r.add((name+":"+str(index),str(arg)))
+        r.append((name+":"+str(index),str(arg)))
         #print r
     elif isinstance(event, Abort):
       (name, fields) = event.GetTypedName()
-      r.add((name+":eip",str(fields[0])))
+      r.append((name+":eip",str(fields[0])))
     
     elif isinstance(event, Exit):
       (name, fields) = event.GetTypedName()
-      r.add((name,str(())))
+      r.append((name,str(())))
     
     elif isinstance(event, Crash):
       (name, fields) = event.GetTypedName()
-      r.add((name+":eip",str(fields[0])))
+      r.append((name+":eip",str(fields[0])))
 
     elif isinstance(event, Signal):
       #assert(0)
@@ -46,9 +46,9 @@ class Printer:
 
       if name == "SIGSEGV":
         #print fields[0]
-        r.add((name+":addr",str(fields[0])))
+        r.append((name+":addr",str(fields[0])))
       else:
-        r.add((name,str(fields[0])))
+        r.append((name,str(fields[0])))
 
     return r
 
@@ -158,37 +158,38 @@ class Printer:
 
 
 class DataPrinter:
-  def __init__(self, filename,pname,classes):
+  def __init__(self, filename,pname,classes,min_size=10):
     self.tests = set()
     self.file = open(filename, "a")
     self.pname = pname
+    self.min_size = min_size
     self.classes = copy.copy(classes)
     self.writer = csv.writer(self.file, delimiter='\t')
 
   def preprocess(self, event):
 
-    r = set()
+    r = list()
     if isinstance(event, Call):
       #print event.name, event.param_types, event.ret, event.retaddr, event.retvalue
       (name, args) = event.GetTypedName()
 
       #r.add((name+":ret_addr",str(args[0])))
-      r.add((name+":ret_val",str(args[1])))
+      r.append((name+":ret_val",str(args[1])))
 
       for (index, arg) in enumerate(args[2:]):
-        r.add((name+":"+str(index),str(arg)))
+        r.append((name+":"+str(index),str(arg)))
         #print r
     elif isinstance(event, Abort):
       (name, fields) = event.GetTypedName()
-      r.add((name+":eip",str(fields[0])))
+      r.append((name+":eip",str(fields[0])))
     
     elif isinstance(event, Exit):
       (name, fields) = event.GetTypedName()
-      r.add((name,str(())))
+      r.append((name,str(())))
     
     elif isinstance(event, Crash):
       (name, fields) = event.GetTypedName()
-      r.add((name+":eip",str(fields[0])))
+      r.append((name+":eip",str(fields[0])))
 
     elif isinstance(event, Signal):
       #assert(0)
@@ -196,9 +197,9 @@ class DataPrinter:
 
       if name == "SIGSEGV":
         #print fields[0]
-        r.add((name+":addr",str(fields[0])))
+        r.append((name+":addr",str(fields[0])))
       else:
-        r.add((name,str(fields[0])))
+        r.append((name,str(fields[0])))
 
     return r
 
@@ -210,7 +211,9 @@ class DataPrinter:
     
     self.original_events = r
     self.original_class = self.find_class(self.original_events)
-    x = hash(tuple(events))
+    
+    x = hash(tuple(self.original_events))
+    #print "Adding", tuple(events), x
     self.tests.add(x)
 
   def find_class(self,events):
@@ -232,28 +235,31 @@ class DataPrinter:
     events = r
 
     x = hash(tuple(events))
+    
     if (x in self.tests):
       return
  
     self.tests.add(x)
+    
+    if len(self.original_events) >= self.min_size:
+     
+      # f(m, original_events) = new_class
 
-    # f(m, original_events) = new_class
+      print self.pname+"\t"+str(delta)+"\t", 
+      for x,y in self.original_events:
+        print x+"="+y+" ",
 
-    print self.pname+"\t"+str(delta)+"\t", 
-    for x,y in self.original_events:
-      print x+"="+y+" ",
-      #assert(not ("abort" in str(x)))  
+      print "\t"+str(self.find_class(events))+"\n",
+  
+    if len(events) >= self.min_size:
+    
+      # f(-m, events) = old_class
+      delta.inv()
 
-    print "\t"+str(self.find_class(events))+"\n",
-   
-    # f(-m, events) = old_class
-    delta.inv()
+      print self.pname+"\t"+str(delta)+"\t", 
+      for x,y in events:
+        print x+"="+y+" ",
 
-    print self.pname+"\t"+str(delta)+"\t", 
-    for x,y in events:
-      print x+"="+y+" ",
-      #assert(not ("abort" in str(x)))  
-
-    print "\t"+str(self.original_class)+"\n",
+      print "\t"+str(self.original_class)+"\n",
 
     return
